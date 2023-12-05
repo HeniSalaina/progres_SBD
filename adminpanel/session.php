bd057a7274bcdb6d3dc84d854437748a9eceaf05
HeniSalaina <143697736+HeniSalaina@users.noreply.github.com>
2023-12-05 18:38:43 +0700

    Delete adminpanel directory

--- adminpanel/session.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if ($_SESSION['login'] == false) {
-    header('location: login.php');
-    exit(); // Pastikan untuk keluar setelah mengarahkan pengguna ke halaman login
-}
-?>
